6c6632c935080eac1811b1c9c09bc60367819cbb
sahil saini <sahil.saini@educateapp.in>
2024-10-04 17:57:32 +0530

    add get route

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 import axios from "axios";
 
 export default async function Home() {
-    const data = await axios.get('http://localhost:3000/api/home'); // Adjust for your environment
+    const data = await axios.get('/api/home'); // Adjust for your environment
 
     return (
         <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
